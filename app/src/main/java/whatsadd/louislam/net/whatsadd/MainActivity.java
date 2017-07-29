e8650fe71398bcabce78e5d8d71711f8b816ca28
LouisLam <louislam@users.noreply.github.com>
2017-07-30 00:01:58 +0800

    init

---
+++ app/src/main/java/whatsadd/louislam/net/whatsadd/MainActivity.java
@@ -0,0 +1,21 @@
+package whatsadd.louislam.net.whatsadd;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+    }
+
+}
