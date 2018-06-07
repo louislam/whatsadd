071eb0dca21ad9545e68bc7f4e4f30a0c61035c5
Louis Lam <louislam@users.noreply.github.com>
2018-06-07 10:48:32 +0800

    update

--- app-base/src/main/java/net/louislam/whatsadd/MainActivity.java
+++ app-base/src/main/java/net/louislam/whatsadd/MainActivity.java
@@ -2,17 +2,18 @@ package net.louislam.whatsadd;
 
 import android.app.AlertDialog;
 import android.content.ActivityNotFoundException;
+import android.content.ComponentName;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Html;
-import android.text.SpannableString;
 import android.text.method.LinkMovementMethod;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -27,9 +28,18 @@ import net.louislam.android.L;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.List;
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements DialogInterface.OnClickListener{
+
+    private AlertDialog settingDialog;
+    private AlertDialog askDialog;
+
+    public final static String WHATSAPP = "com.whatsapp";
+    public final static String WHATSAPP_BUSINESS = "com.whatsapp.w4b";
+    private EditText areaCode;
+    private EditText number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,8 +51,8 @@ public class MainActivity extends AppCompatActivity {
 
         //L.alert(this, Locale.getDefault().getDisplayLanguage());
 
-        final EditText areaCode = (EditText) findViewById(R.id.areaCode);
-        final EditText number = (EditText) findViewById(R.id.number);
+        areaCode = (EditText) findViewById(R.id.areaCode);
+        number = (EditText) findViewById(R.id.number);
         ImageButton button = (ImageButton) findViewById(R.id.button);
 
         number.setOnKeyListener(new View.OnKeyListener() {
@@ -68,6 +78,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        String defaultApp = L.getString(MainActivity.this, "defaultApp");
+
+        if (defaultApp == null) {
+            L.storeString(MainActivity.this, "defaultApp", "2");
+        }
+
         String areaCodeDefault = L.getString(MainActivity.this, "areaCode");
 
         if (areaCodeDefault == null) {
@@ -82,33 +98,42 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent browse = null;
 
-                String areaCodeString = areaCode.getText().toString().trim();
-                String numberString = number.getText().toString().trim();
-
-                if ( areaCodeString.equals("") || numberString.equals("")) {
-                    Toast.makeText(MainActivity.this, "Please input the area code and phone number.",
-                            Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
                 try {
-                    String url = "https://api.whatsapp.com/send?phone=" + URLEncoder.encode(areaCodeString + numberString, "utf-8");
-                    Log.v("URL", url);
+                    //String url = "https://api.whatsapp.com/send?phone=" + URLEncoder.encode(areaCodeString + numberString, "utf-8");
+                    //Log.v("URL", url);
+                    //browse = new Intent( Intent.ACTION_VIEW , Uri.parse(url));
 
-                    browse = new Intent( Intent.ACTION_VIEW , Uri.parse(url));
-                    L.storeString(MainActivity.this, "areaCode", areaCodeString);
-                    number.setText("");
-                    startActivity( browse );
+                    boolean installedWhatsApp = appInstalledOrNot(WHATSAPP);
+                    boolean installedWhatAppBusiness = appInstalledOrNot(WHATSAPP_BUSINESS);
 
+                    if (installedWhatAppBusiness && installedWhatsApp) {
 
-                } catch (UnsupportedEncodingException e) {
-                    L.alert(MainActivity.this, "");
+                        String defaultApp = L.getString(MainActivity.this, "defaultApp");
+
+                        if (defaultApp.equals("0")) {
+                            openWhatsApp(WHATSAPP);
+
+                        } else if (defaultApp.equals("1")) {
+                            openWhatsApp(WHATSAPP_BUSINESS);
+
+                        } else if (defaultApp.equals("2")) {
+                            openAskDialog();
+                        }
+
+                    } else if (installedWhatAppBusiness) {
+                        openWhatsApp(WHATSAPP_BUSINESS);
+                    } else if (installedWhatsApp) {
+                        openWhatsApp(WHATSAPP);
+                    } else {
+                        throw new Exception();
+                    }
+
 
                 } catch (ActivityNotFoundException e) {
                     L.alert(MainActivity.this, getString(R.string.need_browser));
 
                 } catch (Exception e) {
-                    L.alert(MainActivity.this, "Unable to find WhatsApp, please install first.");
+                    L.alert(MainActivity.this, getString(R.string.no_whatsapp));
 
                 }
 
@@ -134,13 +159,16 @@ public class MainActivity extends AppCompatActivity {
                     design = "Designed by";
                 }
 
-                AlertDialog b = new AlertDialog.Builder(MainActivity.this).setMessage( Html.fromHtml("<h2>WhatsAdd</h2>" +
+                AlertDialog b = new AlertDialog.Builder(MainActivity.this).setMessage(Html.fromHtml("<h2>WhatsAdd</h2>" +
                         design + " LouisLam &copy; 2017<br><br>" +
                         "Twitter: <a href=\"https://twitter.com/LouisLam\">@LouisLam</a><br/><br/>" +
-                        "<a href=\"https://louislam.net\">https://louislam.net</a>")).create();
+                        "<a href=\"https://louislam.net/blog\">https://louislam.net</a>")).create();
                 b.show();
 
                 ((TextView) b.findViewById(android.R.id.message)).setMovementMethod(LinkMovementMethod.getInstance());
+                return false;
+            } else if (item.getItemId() == R.id.navigation_settings) {
+                showSettingDialog();
                 return false;
             } else {
                 return true;
@@ -150,5 +178,138 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
-    
+
+    public void openWhatsApp(String packageName) {
+        String areaCodeString = areaCode.getText().toString().trim();
+        String numberString = number.getText().toString().trim();
+
+        try {
+            if ( areaCodeString.equals("") || numberString.equals("")) {
+                Toast.makeText(MainActivity.this, R.string.please_input,
+                        Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            L.storeString(MainActivity.this, "areaCode", areaCodeString);
+            number.setText("");
+
+            Intent intent = new Intent();
+            intent.setComponent(new ComponentName(packageName, "com.whatsapp.TextAndDirectChatDeepLink"));
+            String url = "https://api.whatsapp.com/send?phone=" + URLEncoder.encode(areaCodeString + numberString, "utf-8");
+            Uri uri = Uri.parse(url);
+
+            intent.setData(uri);
+            startActivity(intent);
+
+        } catch (Exception ex) {
+            L.log(ex.toString());
+
+            try {
+                Intent browse;
+                String url = "https://api.whatsapp.com/send?phone=" + URLEncoder.encode(areaCodeString + numberString, "utf-8");
+                Log.v("URL", url);
+
+                browse = new Intent( Intent.ACTION_VIEW , Uri.parse(url));
+                L.storeString(MainActivity.this, "areaCode", areaCodeString);
+                number.setText("");
+                startActivity( browse );
+
+
+            } catch (UnsupportedEncodingException e) {
+                L.alert(MainActivity.this, "");
+
+            } catch (ActivityNotFoundException e) {
+                L.alert(MainActivity.this, getString(R.string.need_browser));
+
+            } catch (Exception e) {
+
+                L.alert(MainActivity.this, "Unable to find WhatsApp, please install first.");
+
+            }
+
+        }
+    }
+
+    public void openAskDialog() {
+
+        // Strings to Show In Dialog with Radio Buttons
+        final CharSequence[] items = {"WhatsApp","WhatsApp Business"};
+
+        // Creating and Building the Dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.choose_app);
+        builder.setSingleChoiceItems(items, -1, this);
+
+        askDialog = builder.create();
+        askDialog.show();
+    }
+
+    public void showSettingDialog() {
+
+        // Strings to Show In Dialog with Radio Buttons
+        final CharSequence[] items = {"WhatsApp","WhatsApp Business", getString(R.string.always_ask)};
+
+        // Creating and Building the Dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.default_app);
+        builder.setSingleChoiceItems(items, Integer.parseInt(L.getString(this, "defaultApp")), this);
+
+        settingDialog = builder.create();
+        settingDialog.show();
+    }
+
+    /**
+     * Settings onClick event
+     */
+    public void onClick(DialogInterface dialog, int item) {
+
+        if (dialog == askDialog) {
+
+            if (item == 1) {
+                openWhatsApp(WHATSAPP_BUSINESS);
+            } else {
+                openWhatsApp(WHATSAPP);
+            }
+
+        } else if (dialog == settingDialog) {
+
+            boolean installedApp = false;
+
+            if (item == 0) {
+                installedApp = appInstalledOrNot(WHATSAPP);
+            } else if (item == 1) {
+                installedApp = appInstalledOrNot(WHATSAPP_BUSINESS);
+            } else if (item == 2) {
+                installedApp  = true;
+            }
+
+            if (installedApp) {
+                L.storeString(MainActivity.this, "defaultApp", "" + item);
+            } else {
+                L.alert(this, getString(R.string.app_not_installed));
+            }
+
+        }
+
+        dialog.dismiss();
+    }
+
+    private boolean appInstalledOrNot(String uri) {
+        PackageManager pm = getPackageManager();
+        List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+
+        // For instant app
+       if (packages.size() == 0) {
+           return true;
+       }
+
+        try {
+            pm.getPackageInfo(uri, PackageManager.GET_ACTIVITIES);
+            return true;
+        } catch (PackageManager.NameNotFoundException e) {
+            L.log("No this app: " + uri);
+            return false;
+        }
+    }
+
 }
