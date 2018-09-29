81f27ad2ec33068f2d13875303a1329090b055f9
LouisLam <louislam@users.noreply.github.com>
2018-09-29 23:31:26 +0800

    store the history

--- app-base/src/main/java/net/louislam/whatsadd/HistoryAdapter.kt
+++ app-base/src/main/java/net/louislam/whatsadd/HistoryAdapter.kt
@@ -5,8 +5,14 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.gson.Gson
 import kotlinx.android.synthetic.main.history_item.view.*
+import net.louislam.android.LStorage
 import java.util.*
+import com.google.gson.reflect.TypeToken
+import net.louislam.android.L
+import kotlin.collections.ArrayList
+
 
 class HistoryAdapter(private val context: Context) : RecyclerView.Adapter<ViewHolder>() {
     companion object {
@@ -14,15 +20,28 @@ class HistoryAdapter(private val context: Context) : RecyclerView.Adapter<ViewHo
         const val ITEM = 1
     }
 
-    private val items : ArrayList<Phone> = ArrayList()
+    private val items : ArrayList<Phone>
+    private val gson = Gson()
 
     init {
-        items.add(Phone("", "", Date()))
+        val listType = object : TypeToken<ArrayList<Phone>>() {}.type
+        val json = LStorage.getString(context, "history");
+
+        if (json == null) {
+            items = ArrayList()
+            items.add(Phone("", "", Date()))
+        } else {
+            items = gson.fromJson(json, listType);
+        }
     }
 
+    /**
+     * Add a item
+     */
     fun add(phone : Phone) {
         items.add(1, phone)
         this.notifyItemInserted(1);
+        save();
     }
 
     override fun getItemCount(): Int {
@@ -47,18 +66,21 @@ class HistoryAdapter(private val context: Context) : RecyclerView.Adapter<ViewHo
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         if (holder.itemViewType == ITEM) {
-            holder.itemView.historyItem.text = items.get(position).toString()
-            holder.itemView.dateView.text = items.get(position).getTimeAgo(context)
+            holder.itemView.historyItem.text = items[position].toString()
+            holder.itemView.dateView.text = items[position].getTimeAgo(context)
         }
     }
 
     fun removeAt(position: Int) {
         items.removeAt(position)
         notifyItemRemoved(position)
+        save();
     }
 
+    fun save() {
+        L.storeString(context, "history", gson.toJson(items))
+    }
 
 }
 
-class ViewHolder (view: View) : RecyclerView.ViewHolder(view) {
-}
+class ViewHolder (view: View) : RecyclerView.ViewHolder(view)
